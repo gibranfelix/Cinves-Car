14f09f7aeab682978caf0c7c777585e8662f9976
unknown <gibranfelix>
2015-06-02 01:32:20 -0500

    Proyecto sin el peaton

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ Controller = Vissim.Net.SignalControllers.ItemByKey(SC_number)
 
 PrincipalGroup = Controller.SGs.ItemByKey(1)
 SecondaryGroup = Controller.SGs.ItemByKey(2)
-PedestrianGroup = Controller.SGs.ItemByKey(3)
+#PedestrianGroup = Controller.SGs.ItemByKey(3)
 index = 0
 while Vissim.Simulation.SimulationSecond <= 1000:
     time = 15
@@ -25,7 +25,7 @@ while Vissim.Simulation.SimulationSecond <= 1000:
             time = 40
             PrincipalGroup.SetAttValue("State", "GREEN")
             SecondaryGroup.SetAttValue("State", "RED")
-            PedestrianGroup.SetAttValue("State", "RED")
+            #PedestrianGroup.SetAttValue("State", "RED")
             Cinvescar.runseconds(time)
 
             PrincipalGroup.SetAttValue("State", "AMBER")
@@ -36,7 +36,7 @@ while Vissim.Simulation.SimulationSecond <= 1000:
             time = totalcars * 1.2
             PrincipalGroup.SetAttValue("State", "GREEN")
             SecondaryGroup.SetAttValue("State", "RED")
-            PedestrianGroup.SetAttValue("State", "RED")
+            #PedestrianGroup.SetAttValue("State", "RED")
             Cinvescar.runseconds(time)
 
             PrincipalGroup.SetAttValue("State", "AMBER")
@@ -46,12 +46,12 @@ while Vissim.Simulation.SimulationSecond <= 1000:
         index = ((index + 1) % 3)
     elif index == 1:
         totalcars = len(Cinvescar.getvehicles(lanes[1][0])) + len(Cinvescar.getvehicles(lanes[1][1]))
-        print "Secundario: "+ str(totalcars)
+        print "Secundario: " + str(totalcars)
         if totalcars >= 70:
             time = 40
             SecondaryGroup.SetAttValue("State", "GREEN")
             PrincipalGroup.SetAttValue("State", "RED")
-            PedestrianGroup.SetAttValue("State", "RED")
+            #PedestrianGroup.SetAttValue("State", "RED")
             Cinvescar.runseconds(time)
 
             SecondaryGroup.SetAttValue("State", "AMBER")
@@ -62,26 +62,14 @@ while Vissim.Simulation.SimulationSecond <= 1000:
             time = totalcars * 1.2
             SecondaryGroup.SetAttValue("State", "GREEN")
             PrincipalGroup.SetAttValue("State", "RED")
-            PedestrianGroup.SetAttValue("State", "RED")
+            #PedestrianGroup.SetAttValue("State", "RED")
             Cinvescar.runseconds(time)
 
             SecondaryGroup.SetAttValue("State", "AMBER")
             Cinvescar.runseconds(3)
 
             SecondaryGroup.SetAttValue("State", "RED")
-        index = ((index + 1) % 3)
-    elif index == 2:
-        time = 15
-        PedestrianGroup.SetAttValue("State", "GREEN")
-        PrincipalGroup.SetAttValue("State", "RED")
-        SecondaryGroup.SetAttValue("State", "RED")
-        Cinvescar.runseconds(time)
-
-        PedestrianGroup.SetAttValue("State", "AMBER")
-        Cinvescar.runseconds(3)
-
-        PedestrianGroup.SetAttValue("State", "RED")
-        index = ((index + 1) % 3)
+        index = ((index + 1) % 2)
 
 
 
